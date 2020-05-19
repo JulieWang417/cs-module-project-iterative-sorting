fcab2a10fa80a3ed4ca7ae3f65fd0276a0ad69a8
Julie_Wang <julie@Julie-Wangs-iMac.local>
2020-05-18 21:51:21 -0700

    finished

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,31 +1,71 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
-
+    # Iterate through array
+    
+    for i in range(0, len(arr)-1):
+        # Find the minimum element in remaining  
+        # unsorted array
+        min_index = i
+        # Iterate from 'i'+1 to end of array
+        for j in range(i+1, len(arr)):
+            #Set min-index to 'j' if less than index-'i'
+            if (arr[j] < arr[min_index]):
+                min_index = j
+        # Swap the found minimum element with the first element 
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    # Iterate through array
+    n = len(arr)
+    # Perform n-1 bubble operations on the sequence, Traverse through all array elements
+    for i in range(n - 1) : # range(n) also work but outer loop will repeat one time more than needed. 
+        # Bubble the largest item to the end.
+        for j in range(0, n-i-1): # Last i elements are already in place 
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element
+            if arr[j] > arr[j+1]: 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
+    # Handle edge cases (empty arrays and negative numbers)
+    if len(arr) < 1:
+        return arr
+    if min(arr) < 0:
+        return "Error, negative numbers not allowed in Count Sort"
+    # Create count array and hold each number's count
+    count = [0 for _ in range(max(arr)+1)]
+    for i in arr:
+        count[i] += 1
+    print(count)
+    # Have count array store total counts up to each index
+    total = 0
+    for i in range(len(count)):
+        count[i], total = (total, count[i]+total)
+    print(count)
+    # Set out array in order by each number's value in the count array
+    arr_out = [0]*len(arr)
+    for i in arr:
+        arr_out[count[i]] = i
+        count[i] += 1
 
     return arr
+
+"""
+def insertionSort(alist):
+    for index in range(1,len(alist)):
+        currentvalue = alist[index] 
+        position = index
+        while position>0 and alist[position-1]>currentvalue:
+            alist[position]=alist[position-1]
+            position = position-1
+            alist[position]=currentvalue
+"""
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,51 @@
 def linear_search(arr, target):
     # Your code here
+    for i,num in enumerate(arr):
+        # if the target is in the ith element,return True
+        if num == target:
+            return i  
 
-
-    return -1   # not found
+    return -1   # False, if not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    # Start with the entire sequence of elements.
+    low = 0
+    high = len(arr) - 1
+    # Repeatedly subdivide the sequence in half until the target is found.
+    while low <= high :
+        # Find the midpoint of the sequence.
+        mid = (high + low) // 2
+        # Does the midpoint contain the target?
+        if arr[mid] == target : 
+            return mid
+        # Or does the target precede the midpoint? Narrow it down to the lower half of current section
+        elif target < arr[mid] : 
+            high = mid - 1
+        # Or does it follow the midpoint? Narrow it down to the upper half of current section
+        else :
+            low = mid + 1
+    # If the sequence cannot be subdivided further, we're done.
+    return -1
 
-    # Your code here
 
+"""
+# Write an recursive implementation of Binary Search
+def binarySearch_recursive(arr, item): 
+    if len(arr) == 0 :
+        return False 
+    else:
+        mid = len(arr)//2
+        if arr[mid] == item:
+            return True 
+        else:
+            if item < arr[mid]:
+                return binarySearch (arr[:mid],item)
+            else:
+                return binarySearch (arr[mid+1:],item)
+test_arr = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binarySearch(test_arr, 3)) 
+print(binarySearch(test_arr, 13))
 
-    return -1  # not found
+"""
